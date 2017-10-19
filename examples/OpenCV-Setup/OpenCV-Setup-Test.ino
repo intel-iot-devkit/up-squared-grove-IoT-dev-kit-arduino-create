d268f6ac1c0417fe7983a8ac819b8bc8ee23d479
Joseph Butler <joseph.butler@intel.com>
2017-10-18 17:18:25 -0700

    opencv code update

---
+++ examples/OpenCV-Setup/OpenCV-Setup-Test.ino
@@ -0,0 +1,17 @@
+#include "ArduinoOpenCV.h"
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/videoio.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void setup()
+{
+    cout << "OpenCV Version:  " << CV_VERSION << endl;
+}
+
+void loop() {}
+
