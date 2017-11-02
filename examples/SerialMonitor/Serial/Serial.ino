37b195ef440a4118c431a2a8409efb0802e053fd
Joseph Butler <joseph.butler@intel.com>
2017-11-01 21:46:50 -0700

    Update Serial.ino

--- examples/SerialMonitor/Serial/Serial.ino
+++ examples/SerialMonitor/Serial/Serial.ino
@@ -1,4 +1,17 @@
+/*  
+*  Copyright (c) 2017 Intel Corporation.
+*  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+*/
 
+/*
+  Serial
+  Learn how to set up a serial monitor. You’ll use built-in functions to set up, transmit, and receive data over serial, which is very useful for debugging.
+  
+  Make sure you open the Monitor after uploading this sketch.  Serial only works on UP Squared devices connected over micro USB.
+  
+  https://github.com/intel-iot-devkit/up-squared-grove-IoT-dev-kit-arduino-create/tree/master/examples/SerialMonitor/Serial
+  
+*/
 
 int incomingByte = 0;   // for incoming serial data
 void setup() {
