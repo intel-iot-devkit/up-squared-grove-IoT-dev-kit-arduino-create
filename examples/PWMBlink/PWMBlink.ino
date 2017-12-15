0f1b1cb53157269e32a4833945925c0042118ed9
Blain <blainc89@gmail.com>
2017-12-15 12:05:38 -0800

    Update pin, doc
    Update pin number and documentation at top. Change subplatform back to MRA_GROVEPI, and change DELAY_TIME. Also, change max value from 1023 to 255.

--- examples/PWMBlink/PWMBlink.ino
+++ examples/PWMBlink/PWMBlink.ino
@@ -7,7 +7,7 @@ Licensed under the MIT license. See LICENSE file in the project root for full li
 
   Example using PWM on Arduino Create using UPM.
 
-  Cycles through all PWM output values on a single pin from 0 to 1024 and back to 0 over the course of approximately four seconds, then repeats.
+  Cycles through all PWM output values on a single pin from 0 to 255 and back to 0 over the course of approximately two seconds, then repeats.
 
 */
 
@@ -15,19 +15,20 @@ Licensed under the MIT license. See LICENSE file in the project root for full li
 
 #include "upm_utilities.h"
 
-#define LED_PIN 516
-#define DELAY_TIME 2
+#define LED_PIN 517
+#define DELAY_TIME 4
 
 void setup() {
-	mraa_add_subplatform(MRAA_GENERIC_FIRMATA, "/dev/ttyS1"); //For testing. This is the way it's done in another example, but in others it's (MRAA_GROVEPI, "0"). I don't know which is correct, so I'm testing both.
+	pinMode(LED_PIN, OUTPUT);
+	mraa_add_subplatform(MRAA_GROVEPI, "0");
 }
 
 void loop() {
-	for (int i = 0; i < 1023; i++) {
+	for (int i = 0; i < 255; i++) {
 		analogWrite(LED_PIN, i);
 		delay(DELAY_TIME);
 	}
-	for (int i = 1023; i > 0; i--) {
+	for (int i = 255; i > 0; i--) {
 		analogWrite(LED_PIN, i);
 		delay(DELAY_TIME);
 	}
