5ec42d99048c68e688a2a58cb061bd60c0e2c98d
msgtfrank <frankww1@gmail.com>
2017-12-16 23:37:24 -0700

    Fixed SEGFAULT issue in INO
    Signed-off-by: msgtfrank <frankww1@gmail.com>

--- examples/GroveTemperatureAndHumidity/GroveTemperatureAndHumidity.ino
+++ examples/GroveTemperatureAndHumidity/GroveTemperatureAndHumidity.ino
@@ -13,52 +13,61 @@ Licensed under the MIT license. See LICENSE file in the project root for full li
 #include <th02.hpp>
 #include "upm_utilities.h"
 
-//Create variables to hold the temperature and humidity
-float temperature = 0.0;
-float humidity = 0.0;
-//Create variable to hold the temperature modifier
-char tempModifier = 'F'; //Modifier allows conversion to Fahrenheit or Kelvin, Base is Celsius
-//Create sensor
-upm::TH02 sensor = upm::TH02();
+
 //Prototype function
 float ModTemp(char, float);
 
 void setup() {
   //Begin serial terminal
-  //If you are using the UP^2 board via serial port (COM, tty, etc) change the DebugSerial method with Serial in all instances
+  //If you are using the UP^2 board via serial port (COM, tty, etc) exchange the DebugSerial method with Serial in all instances
   DebugSerial.begin(9600);
+  
 }
 
 void loop() {
+  //Create sensor
+  upm::TH02 sensor;
+  
+  //Create variables to hold the temperature and humidity
+  float temperature = 0.0;
+  float humidity = 0.0;
+  
+  //Create buffer
+  char buff[10];
+  
+  //Create variable to hold the temperature modifier
+  char tempModifier = 'F'; //Modifier allows conversion to Fahrenheit or Kelvin, Base is Celsius
+  
   //Create a string to hold our message
   String outputMessage = "";
   
   //Get temperature
   temperature = sensor.getTemperature();
+  
   //Get humidity
   humidity = sensor.getHumidity();
   //Perform math to change temperature unit
   temperature = ModTemp(tempModifier, temperature);
   
-  //Build temperature line of message "Temperature: (current temp) °(Unit)"
-  outputMessage = "Temperature: " + String(temperature, 2) + " " + char(223) + tempModifier;
-  //Build humidity line of message "Humidity: (current RH)% Relative Humidity"
-  outputMessage += "\nHumidity: " + String(humidity, 2) + "% Relative Humidity";
-  //Build spacing line
-  outputMessage += "\n\n------------------------------\n";
+  //Build output message for temperature
+  outputMessage += (String(temperature) + " degrees " + tempModifier + "\n");
+  //Build output message for humidity
+  outputMessage += (String(humidity) + " percent relative humidity\n");
+  //Build shiny break bar
+  outputMessage += "---------------------------------------\n\n";
   
-  //Output our message
+  //Print information
   DebugSerial.println(outputMessage);
-  
+
   //Sleep 3 seconds between readings
   delay(3000);
 }
 
 float ModTemp(char modifier, float modifiedTemp){
-  if (modifier = 'F'){ //Perform math to change Celsius to Fahrenheit
-    modifiedTemp = ((modifiedTemp * (9/5)) + 32);
+  if (modifier == 'F'){ //Perform math to change Celsius to Fahrenheit
+    modifiedTemp = ((modifiedTemp * 1.8) + 32);
   }
-  else if (modifier = 'K'){ //Perform math to change Celsius to Kelvin
+  else if (modifier == 'K'){ //Perform math to change Celsius to Kelvin
     modifiedTemp += 273.15;
   }
   return modifiedTemp;
